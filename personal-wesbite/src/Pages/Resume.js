dccc984de5c91ca88b2e74fe64665042ab77bee9
Raiyan Ausaf <raiyanausaf14@icloud.com>
2023-05-22 17:05:32 -0700

    adding pages

--- personal-wesbite/src/Pages/Resume.js
+++ personal-wesbite/src/Pages/Resume.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Document,Page } from 'react-pdf/dist/esm/entry.webpack';
+import pdfFile from '../berkeleyResume.pdf'
+import "react-pdf/dist/esm/Page/TextLayer.css";
 
 export default function Resume() {
   return (
-    <div>Resume</div>
+    <div>
+      <Document file={pdfFile}>
+        <Page pageNumber={1} />
+      </Document>
+    </div>
   )
 }
