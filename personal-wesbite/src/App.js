36768d14032a36b611bb23d7de9db7aba5ceb19e
Raiyan Ausaf <raiyanausaf14@icloud.com>
2023-05-25 20:13:39 -0700

    basic user authentification

--- personal-wesbite/src/App.js
+++ personal-wesbite/src/App.js
@@ -6,10 +6,38 @@ import Contact from "./Pages/ContactMe.js";
 import About from "./Pages/AboutMe.js";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./Components/Header.js";
+import Login from "./Pages/Login.js";
+import { useState } from 'react';
 
 export const App = () => {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <>
+    <div
+      className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      aria-hidden="true">
+      <div
+        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#9BE6B1] to-[#2C3A47] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+        style={{
+        clipPath:
+          'circle(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+        }} />
+    </div>
+    <div
+      className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+      aria-hidden="true" >
+      <div
+        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#9BE6B1] to-[#2C3A47] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+        style={{
+          clipPath:
+            'circle(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+        }}/>
+    </div>
     <Header />
     <Routes>
       <Route path="/" element={<Main />} exact />
@@ -17,6 +45,7 @@ export const App = () => {
       <Route path="/projects" element={<Projects />} />
       <Route path="/contactme" element={<Contact />} />
       <Route path="/aboutme" element={<About />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
     </>
   )
